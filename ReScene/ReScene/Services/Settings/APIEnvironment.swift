eb25910907e1b19ddebd5a20707b63447ecff140
Kai Chen <remlostime@gmail.com>
2026-03-11 18:17:55 -0700

    AWS api support

--- ReScene/ReScene/Services/Settings/APIEnvironment.swift
+++ ReScene/ReScene/Services/Settings/APIEnvironment.swift
@@ -8,21 +8,25 @@ import Foundation
 /// Represents the available backend API environments.
 enum APIEnvironment: String, Codable, CaseIterable, Sendable {
     case dev
-    case prod
+    case google
+    case aws
 
     var baseURL: URL {
         switch self {
         case .dev:
             URL(string: "http://localhost:8080")!
-        case .prod:
+        case .google:
             URL(string: "https://rescene-api-568316754281.us-west1.run.app")!
+        case .aws:
+            URL(string: "https://twybjyvj4w.us-east-1.awsapprunner.com")!
         }
     }
 
     var displayName: String {
         switch self {
         case .dev: "Dev"
-        case .prod: "Prod"
+        case .google: "Google"
+        case .aws: "AWS"
         }
     }
 }
